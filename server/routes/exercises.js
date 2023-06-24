1c17b2088358a45301d4fc551040f7029cdca835
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-25 03:14:06 +0800

    More routes under /exercises
    1. Added 3 more endpoints to exercises.js
    	1.1. Get by ID
    	1.2. Delete by ID
    	1.3. Update by ID
    2. Beau says we're done with the backend and we're now going to React
    3. Lesgawww

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const Exercises = require('../models/exercise.model.js');
+const Exercise = require('../models/exercise.model.js');
 
 router.route('/').get(
   (req, res) => {
-    Exercises.find()
+    Exercise.find()
       .then(exercises => res.json(exercises))
       .catch(err => res.status(400).json(`Error: ${err}`));
   }
@@ -16,7 +16,7 @@ router.route('/add').post(
     const duration = Number(req.body.duration);
     const date = Date.parse(req.body.date);
 
-    const newExercise = new Exercises(
+    const newExercise = new Exercise(
       {
         username,
         description,
@@ -27,6 +27,41 @@ router.route('/add').post(
 
     newExercise.save()
       .then(() => res.json('Exercise added!'))
+      .catch(err => res.status(400).json(`Error: ${err}`));
+  }
+)
+
+router.route('/:id').get(
+  (req,res) => {
+    Exercise.findById(req.params.id)
+      .then(exercise => res.json(exercise))
+      .catch(err => res.status(400).json(`Error: ${err}`));
+  }
+)
+
+router.route('/:id').delete(
+  (req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise deleted.'))
+      .catch(err => res.status(400).json(`Error: ${err}`));
+  }
+)
+
+router.route('/update/:id').post(
+  (req, res) => {
+    Exercise.findById(req.params.id)
+      .then(
+        exercise => {
+          exercise.username = req.body.username;
+          exercise.description = req.body.description;
+          exercise.duration = Number(req.body.duration);
+          exercise.date = Date.parse(req.body.date);
+
+          exercise.save()
+            .then(() => res.json('Exercise updated!'))
+            .catch(err => res.status(400).json(`Error: ${err}`));
+        }
+      )
       .catch(err => res.status(400).json(`Error: ${err}`));
   }
 )
