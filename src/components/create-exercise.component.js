c98ef0578bdeb07edadaa81eb3955302106b89e8
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-26 01:23:41 +0800

    create-exercise.component.js ready
    1. Prepared create-exercise.component.js and the page is now ready
    2. So a big problem with the tutorial is it's using Class components for React, when we mainly use Functional components now
    3. I simply converted the code with this in mind, and it definitely works wonders even if it's not exactly 1-to-1 (just not sure if I missed something so I can't definitively say)
    4. I have to thank Angela's course and rigorous practice of React forms because now it's pretty much second nature to me writing what I think are the essentials of forms, states associated and the functions that handle the state changes
    5. Built all of those in #4, following the tutorial for the exact html and Bootstrap classings
    6. Beyond that, I also utilized React-DatePicker following the tutorial, which is an interesting (albeit unnecessary) addition to my toolkit
    	6.1. It's basically just a much better looking and possibly better functioning input type=date component

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,9 +1,104 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function CreateExercise() {
+  const [newExercise, setNewExercise] = useState(
+    {
+      username: 'test user',
+      description: '',
+      duration: 0,
+      date: new Date(),
+    }
+  );
+
+  const [users, setUsers] = useState(
+    ['test user']
+  );
+  
+  function onInputChange(event) {
+    const {name, value} = event.target;
+
+    console.log(`${name} = ${value}`); 
+
+    setNewExercise(prevValue => {
+      return {...prevValue, [name]: value};
+    })
+  }
+
+  function onChangeDate(date) {
+    setNewExercise(prevValue => {
+      return {...prevValue, 'date': date};
+    })
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    console.log(newExercise); 
+
+    window.location = '/';
+  }
+
   return (
     <div>
-      <p>You are on the Create Exercise component!</p>
+      <h3>Create New Exercise Log</h3>
+      <form>
+        <div className="form-group">
+          <label>Username: </label>
+          <select 
+            className="form-control"
+            value={newExercise.username}
+            onChange={onInputChange}
+            required
+          >
+            {
+              users.map(
+                (user, index) => {
+                  return (
+                    <option
+                      key={index}
+                      value={user}
+                    >
+                      {user}
+                    </option>
+                  )
+                }
+              )
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <textarea
+            className="form-control"
+            required
+            onChange={onInputChange}
+            type="text"
+            name="description"
+            value={newExercise.description}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration: </label>
+          <input
+            className="form-control"
+            required
+            onChange={onInputChange}
+            type="number"
+            name="duration"
+            value={newExercise.duration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <DatePicker
+            selected={newExercise.date} 
+            onChange={onChangeDate}
+          />
+        </div>
+        <button onClick={onSubmit}>Submit</button>
+      </form>
     </div>
   );
 };
