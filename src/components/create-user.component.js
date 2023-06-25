9f686059c9bef627bcc559d774c44ec92d9486dc
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-26 02:22:51 +0800

    create-user.component.js ready
    1. Prepared create-user.component.js and the page is now ready
    2. Set up the states and the jsx as well as the functions that handle state changes
    	2.1. It's quite similar to create-exercise.component.js so not much work is needed
    3. Olgood

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,9 +1,53 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 
 export default function CreateUser() {
+  const [newUser, setNewUser] = useState(
+    {
+      username: ''
+    }
+  );
+
+  function onInputChange(event) {
+    const {value} = event.target;
+
+    setNewUser(
+      {
+        username: value
+      }
+    );
+  };
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    console.log(newUser); 
+
+    setNewUser(
+      {
+        username: ''
+      }
+    );
+  };
+
   return (
     <div>
-      <p>You are on the Create User component!</p>
+      <h3>Create New User</h3>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Username: </label>
+          <input
+            className="form-control"
+            required
+            onChange={onInputChange}
+            type="text"
+            name="username"
+            value={newUser.username}
+          />
+        </div>
+        <div className="mb-3">
+          <button className="btn btn-primary" onClick={onSubmit}>Submit</button>
+        </div>
+      </form>
     </div>
   );
 };
