7a6705930f06fb2ae096c1876825faa79632508d
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-26 02:31:33 +0800

    Removed Component fetch from 'react' package
    1. Sooo in the tutorial, Component is being imported from 'react', but this is only needed when building class components
    2. Not what I'm doing, so I just had this import removed from the 5 component files

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 
 export default function CreateUser() {
   const [newUser, setNewUser] = useState(
