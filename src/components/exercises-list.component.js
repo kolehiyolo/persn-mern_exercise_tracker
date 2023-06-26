39a719283f3a66a276d733bb4d3a67901588123f
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-26 16:12:46 +0800

    Exercise list viewing and deleting ready
    1. Cool challenge this was, where I did a lot of debugging and consulting ChatGPT and Google since there are quite a lot of outdated stuff with the tutorial
    2. Exercises list all good now with the following functionalities:
    	2.1. Exercises are being fetched at mount with a useEffect calling fetchExercises()
    		2.1.1. I opted to have the fetch call in a function since the component will do this frequently when the user deletes exercises (will explain)
    	2.2. The JSX render is just a simple table with the body automatically filling with a map return
    	2.3. The fetched list is mapped so each list item is rendered as an <Exercise> with relevant data
    	2.4. Each <Exercise> is passed the deleteExercise(id) function, triggered via a button which deletes the relevant exercise from the DB
    	2.5. Once this function is called, fetchExercises() is called again to update the exercises state, re-rendering the UI to reflect the now updated list consistent with the DB
    		2.5.1. This is actually a dilemma since the tutorial opted to immediately re-render the UI by simply removing the corresponding <Exercise> element without ensuring that it's consistent with the DB, and after consulting ChatGPT, I think it best to delete in the DB, fetch the updated list from the DB, then re-render
    3. User can now view all exercises and delete accordingly

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import Exercise from './exercise.component.js';
 
 export default function ExercisesList() {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(
+    () => {
+      fetchExercises();
+    },
+    []
+  );
+
+  function fetchExercises() {
+    console.log(`fetchExercises()`); 
+    axios.get('http://localhost:5000/exercises')
+      .then(
+        res => setExercises(res.data)
+      )
+      .catch(
+        err => res.status(400).json(`Error: ${err}`)
+      );
+  };
+
+  function deleteExercise(id) {
+    axios.delete(`http://localhost:5000/exercises/${id}`)
+      .then(
+        res => {
+          console.log(res.data);
+          fetchExercises();
+        }
+      )
+      .catch(
+        err => res.status(400).json(`Error: ${err}`)
+      );
+  }
+
+  function generateExerciseItem(item) {
+    return (
+      <Exercise
+        exercise={item}
+        deleteExercise={deleteExercise}
+        key={item._id}
+      />
+    )
+  }
+
   return (
     <div>
-      <p>You are on the Exercises List component!</p>
+      <h3>Logged Exercises</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            exercises.map(generateExerciseItem)
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
